37b654d1c4a2f8656271b283162a3bb2401aa61e
yeefongwu <wu564@wisc.edu>
2024-02-24 15:02:06 -0600

    wb5

--- wb5/05-12-01.js
+++ wb5/05-12-01.js
@@ -1,22 +1,44 @@
-//yw
 let canvas = document.getElementById("canvas1");
 if (!(canvas instanceof HTMLCanvasElement))
     throw new Error("Canvas is not HTML Element");
 
 let context = canvas.getContext("2d");
+let table = [];
+let points = [];
 
+function findClosestIndex(table, targetDistance) {
+    let left = 0;
+    let right = table.length - 1;
+  
+    while (left <= right) {
+      let mid = Math.floor((left + right) / 2);
+      if (table[mid] === targetDistance) {
+        return mid;
+      } else if (table[mid] < targetDistance) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+  
+    let leftDiff = targetDistance - table[right];
+    let rightDiff = table[left] - targetDistance;
+  
+    return (leftDiff < rightDiff) ? right : left;
+  }
 
-function calculateBezierPoint(t, p0, p1, p2, p3) {
-    const x = Math.pow(1 - t, 3) * p0.x +
-              3 * Math.pow(1 - t, 2) * t * p1.x +
-              3 * (1 - t) * Math.pow(t, 2) * p2.x +
-              Math.pow(t, 3) * p3.x;
-    const y = Math.pow(1 - t, 3) * p0.y +
-              3 * Math.pow(1 - t, 2) * t * p1.y +
-              3 * (1 - t) * Math.pow(t, 2) * p2.y +
-              Math.pow(t, 3) * p3.y;
-    return {x, y};
+function calculateHermitePoint(u, H0, T0, T1, H1) {
+    const h00 = 2 * u * u * u - 3 * u * u + 1;
+    const h10 = u * u * u - 2 * u * u + u;
+    const h01 = -2 * u * u * u + 3 * u * u;
+    const h11 = u * u * u - u * u;
+
+    const x = h00 * H0.x + h10 * T0.x + h01 * H1.x + h11 * T1.x;
+    const y = h00 * H0.y + h10 * T0.y + h01 * H1.y + h11 * T1.y;
+
+    return { x, y };
 }
+
 
 function calculateDistance(x1, y1, x2, y2) {
     const dx = x2 - x1;
@@ -24,12 +46,6 @@ function calculateDistance(x1, y1, x2, y2) {
     return Math.sqrt(dx * dx + dy * dy);
 }
 
-
-context.bezierCurveTo(50 + (350 - 50) / 6, 150 + (150 - 50) / 6, 350 + (50 - 350) / 6, 150 + (150 - 50) / 6, 350, 150);
-context.bezierCurveTo(350 + (350 - 50) / 6, 150 + (50 - 150) / 6, 350 + (350 - 200) / 6, 50 + (150 - 100) / 6, 350, 50);
-context.bezierCurveTo(350 + (200 - 350) / 6, 50 + (100 - 150) / 6, 200 + (350 - 50) / 6, 100 + (50 - 50) / 6, 200, 100);
-context.bezierCurveTo(200 + (50 - 350) / 6, 100 + (50 - 50) / 6, 50 + (200 - 50) / 6, 50 + (100 - 150) / 6, 50, 50);
-context.bezierCurveTo(50 + (50 - 200) / 6, 50 + (150 - 100) / 6, 50 + (50 - 350) / 6, 150 + (50 - 150) / 6, 50, 150);
 
 let ab0 = {x: 50, y: 150};
 let ab1 = {x: 50 + (350 - 50) / 6, y: 150 + (150 - 50) / 6};
@@ -56,6 +72,19 @@ let ea1 = {x: 50 + (50 - 200) / 6, y: 50 + (150 - 100) / 6};
 let ea2 = {x: 50 + (50 - 350) / 6, y: 150 + (50 - 150) / 6};
 let ea3 = {x: 50, y: 150};
 
+//hermite
+let ab1_modified = {x: 150.0, y: 50.0};
+let bc1_modified = {x: 150.0, y: -50.0};
+let cd1_modified = {x: -75.0, y: -25.0};
+let de1_modified = {x: -150.0, y: 0.0};
+let ea1_modified = {x: -75.0, y: 25.0};
+
+let ab2_modified = {x: 150.0, y: -50.0};
+let bc2_modified = {x: -75.0, y: -25.0};
+let cd2_modified = {x: -150.0, y: 0.0};
+let de2_modified = {x: -75.0, y: 25.0};
+let ea2_modified = {x: 150.0, y: 50.0};
+
 
 context.beginPath();
 
@@ -71,50 +100,51 @@ context.lineWidth = 3;
 context.stroke();
 
 
-let seg1 = calculateDistance(ab0.x, ab0.y, ab1.x, ab1.y) 
-+ calculateDistance(ab1.x, ab1.y, ab2.x, ab2.y)
-+ calculateDistance(ab2.x, ab2.y, ab3.x, ab3.y);
 
-let seg2 = calculateDistance(bc0.x, bc0.y, bc1.x, bc1.y)
-+ calculateDistance(bc1.x, bc1.y, bc2.x, bc2.y)
-+ calculateDistance(bc2.x, bc2.y, bc3.x, bc3.y);
-
-let seg3 = calculateDistance(cd0.x, cd0.y, cd1.x, cd1.y)
-+ calculateDistance(cd1.x, cd1.y, cd2.x, cd2.y)
-+ calculateDistance(cd2.x, cd2.y, cd3.x, cd3.y);
-
-let seg4 = calculateDistance(de0.x, de0.y, de1.x, de1.y)
-+ calculateDistance(de1.x, de1.y, de2.x, de2.y)
-+ calculateDistance(de2.x, de2.y, de3.x, de3.y);
-
-let seg5 = calculateDistance(ea0.x, ea0.y, ea1.x, ea1.y)
-+ calculateDistance(ea1.x, ea1.y, ea2.x, ea2.y)
-+ calculateDistance(ea2.x, ea2.y, ea3.x, ea3.y);
-
-
-let total = seg1+seg2+seg3+seg4+seg5;
-let gap = total/20;
-let count = 0;
-
-for(let i = 0; i < total; i += gap){
-    context.fillStyle = "red";
-    let x, y;
-    let point; 
-
-    if (i < seg1) {
-        point = calculateBezierPoint(i / seg1, ab0, ab1, ab2, ab3);
-    } else if (i < seg2+seg1) {
-        point = calculateBezierPoint((i - seg1) / seg2, bc0, bc1, bc2, bc3);
-    } else if (i < seg3+seg1+seg2) {
-        point = calculateBezierPoint((i - seg1 - seg2) / seg3, cd0, cd1, cd2, cd3);
-    } else if (i < seg4+seg1+seg2+seg3) {
-        point = calculateBezierPoint((i - seg1 - seg2 - seg3) / seg4, de0, de1, de2, de3);
-    } else {
-        point = calculateBezierPoint((i - seg1 - seg2 - seg3 - seg4) / seg5, ea0, ea1, ea2, ea3);
+let increment = 0.0001;
+table.push(0);
+points.push(calculateHermitePoint(0.001,ab0,ab1_modified,ab2_modified,ab3));
+for(let u=increment;u<5;u+=increment){
+    let point;
+    let lastPointIndex = Math.floor(u/increment-1);
+    if(u<1){
+        point = calculateHermitePoint(u,ab0,ab1_modified,ab2_modified,ab3);
+    }else if(u<2){
+        point = calculateHermitePoint(u-1,bc0,bc1_modified,bc2_modified,bc3);
+    }else if(u<3){
+        point = calculateHermitePoint(u-2,cd0,cd1_modified,cd2_modified,cd3);
+    }else if(u<4){
+        point = calculateHermitePoint(u-3,de0,de1_modified,de2_modified,de3);
+    }else{
+        point = calculateHermitePoint(u-4,ea0,ea1_modified,ea2_modified,ea3);
     }
-
-    x = point.x;
-    y = point.y;
-    context.fillRect(x-2, y-2, 5, 5); 
+    points.push(point);
+    let distance = calculateDistance(point.x,point.y,points[lastPointIndex].x,points[lastPointIndex].y);
+    table.push(table[lastPointIndex] + distance);
 }
+
+let arcLength = table[table.length-1];
+// console.log(arcLength);
+// console.log(table);
+// console.log(points);
+
+for(let i = 0;i<arcLength;i+=arcLength/20){
+    context.fillStyle = "red";
+    let u = findClosestIndex(table,i)*increment;
+    let point;
+    if(u<1){
+        point = calculateHermitePoint(u,ab0,ab1_modified,ab2_modified,ab3);
+    }else if(u<2){
+        point = calculateHermitePoint(u-1,bc0,bc1_modified,bc2_modified,bc3);
+    }else if(u<3){
+        point = calculateHermitePoint(u-2,cd0,cd1_modified,cd2_modified,cd3);
+    }else if(u<4){
+        point = calculateHermitePoint(u-3,de0,de1_modified,de2_modified,de3);
+    }else{
+        point = calculateHermitePoint(u-4,ea0,ea1_modified,ea2_modified,ea3);
+    }
+    context.fillRect(point.x-3,point.y-3,5,5);
+}
+
+
 
